ea6948c6dbc3d67156c2e1107d2524384b3ca4b0
Rivas Frits Sasia <fritssasia@gmail.com>
2021-08-26 10:16:34 +0800

    update content, style and responsive

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+$('.custom-file-input').on('change', function () {
+    var fileName = document.getElementById("covid-positive-file").files[0].name;
+    $(this).next('.covid-positive-filename').addClass("selected").html(fileName);
+});
+
+$('.custom-file-input').on('change', function () {
+    var fileName = document.getElementById("covid-negative-file").files[0].name;
+    $(this).next('.covid-negative-filename').addClass("selected").html(fileName);
+});
+
+function covidPositiveFilePreview(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('.covid-positive-file-preview')
+                .attr('src', e.target.result)
+                .width('120px')
+                .height('120px')
+                .css("margin-top", "50px");
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+};
+
+function covidNegativeFilePreview(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('.covid-negative-file-preview')
+                .attr('src', e.target.result)
+                .width('120px')
+                .height('120px')
+                .css("margin-top", "50px");
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+};
+
+$("#select-gender").selectize({
+    sortField: "text",
+});
+
+$("#select-blood-group").selectize({
+    sortField: "text",
+});
+
+$("#select-blood-rhesus").selectize({
+    sortField: "text",
+});
+
+$("#select-city").selectize({
+    sortField: "text",
+});
